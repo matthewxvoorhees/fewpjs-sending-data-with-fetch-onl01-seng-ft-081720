6fdf06dc5ced8e34a99c413033e8b1ca08dbc004
matthewxvoorhees <matthew.crnich@gmail.com>
2020-11-25 15:57:49 -0700

    commit tree 3c56f3a85247aa09592e8e58cf63d6c8b892b8a3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    document.body.innerHTML = object.id;
+  })
+  .catch(function(error) {
+    alert("Failed!");
+    document.body.innerHTML = error.message;
+  })
+}
